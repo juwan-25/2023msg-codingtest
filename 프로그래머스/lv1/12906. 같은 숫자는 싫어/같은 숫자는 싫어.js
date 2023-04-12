936793874855cd877dc02a8d6c263b561b255425
Lee Juwan <83991017+juwan-25@users.noreply.github.com>
2023-04-13 00:04:23 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 13.36 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -2,7 +2,7 @@ function solution(arr)
 {
     let answer = [];
     for(let i=0; i<arr.length; i++){
-        if(arr[i]!=arr[i+1]) answer.push(arr[i]);
+        if(arr[i]!=answer[answer.length - 1] ) answer.push(arr[i]);
     }
     
     return answer;
